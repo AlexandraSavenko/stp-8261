c4695d4a80f96621fd87e28c584b186142aa212d
Sasha Savenko <sashasavenko3@gmail.com>
2025-03-16 14:49:37 +0200

    benefits swiper appears and goes

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -1,11 +1,11 @@
 import Swiper from 'swiper/bundle';
 
 import 'swiper/css/bundle';
-
 let swiperInstance; 
 function handleSwiper() {
-  if (window.innerWidth <= 1200) { 
-    if (!swiperInstance) { // Initialize only if it doesn't exist
+
+  if (window.innerWidth < 1200) { 
+    if (!swiperInstance) { 
       swiperInstance = new Swiper('.benefits-swiper', {
         slidesPerView: 1,
         spaceBetween: 24,
@@ -18,25 +18,12 @@ function handleSwiper() {
           clickable: true,
         },
       });
-            console.dir(swiperInstance)
-
     }
   } else { 
     if (swiperInstance) { 
-      document.querySelector('.benefits-swiper').style.display = 'none';
-      // document.querySelectorAll('.swiper-container').forEach(swiper => {
-      //   if (swiper.swiper) {
-      //     swiper.swiper.destroy(true, true);
-      //   }
-      // });
+swiperInstance.destroy(true, true)
+      swiperInstance = null
       
-      // console.log(`this is swiper instance ${swiperInstance.destroy}`)
-  //     swiperInstance.destroy(false, true);
-  //     document.querySelector('.benefits-swiper').style.display = 'none';
-  // setTimeout(() => {
-  //   document.querySelector('.benefits-swiper').style.display = 'block';
-  // }, 10);
-  // swiperInstance = null;
 
     }
   }
