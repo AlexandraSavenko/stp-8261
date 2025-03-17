a19c1a3837eb257f0b035cdb065ca250996fc61d
Sasha Savenko <159346450+AlexandraSavenko@users.noreply.github.com>
2025-03-17 21:35:10 +0200

    gallery added (#18)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './js/header.js';
 import './js/benefits.js'
  import './js/key-features.js'
-
+import './js/gallery.js'
  import './js/reviews.js';
-// import './js/faq.js';
 import './js/cookie-popup.js';
