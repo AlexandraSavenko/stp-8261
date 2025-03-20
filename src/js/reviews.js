360aaa43ee2642e5ba0fdcb817259da45da00314
Sasha Savenko <sashasavenko3@gmail.com>
2025-03-20 09:41:08 +0200

    gallery images fixed

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -2,16 +2,6 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css';
 import 'swiper/css/parallax';
 
-// const swiper = new Swiper('.swiper-container', {
-//   parallax: true,
-//   slidesPerView: 'auto',
-//   loop: true,
-//   autoplay: {
-//     delay: 3000,
-//     disableOnInteraction: false,
-//   },
-//   speed: 800,
-// });
 
 const swiper = new Swiper('.reviews-swiper', {
  slidesPerView: 1,
